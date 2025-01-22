930fc5950887c6cfbd988442fdb80491dc8a6b35
andrewbouras <bandrew9@vt.edu>
2025-01-22 16:33:43 -0500

    Updates

--- backend/models/ShortenedUrl.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ShortenedUrlSchema = new mongoose.Schema({
-  token: { type: String, required: true, unique: true },
-  originalUrl: { type: String, required: true },
-  accessType: { type: String, enum: ['admin', 'editor', 'view-only'], required: true }
-});
-
-module.exports = mongoose.model('ShortenedUrl', ShortenedUrlSchema);
--- backend/routes/webhookRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleStripeWebhook } = require('../controllers/subscriptionController');
-
-router.post('/', express.raw({ type: 'application/json' }), handleStripeWebhook);
-
-module.exports = router;
